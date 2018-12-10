c6560c9b79e2456663233022dc6fd5746c934699
AP Keerthika <keerthi2592@gmail.com>
2018-12-10 16:21:10 +0530

    Add section and containers

---
+++ components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+section.app-header
+  .container
+    h1 App Header
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass" scoped>
+@import 'assets/styles/includes'
+</style>
